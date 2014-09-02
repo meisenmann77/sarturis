ff430ef2b93d69e130a8bed7a7129ff790de088f
meisenmann77@googlemail.com <meisenmann77@googlemail.com@6970fdc6-4ef0-4c3b-a58b-f90f553653bd>
2014-09-02 18:19:16 +0000

    - Buildumgebung angefangen

---
+++ trunk/build/builder.py
@@ -0,0 +1,83 @@
+'''
+
+   Sarturis is (C) Copyright 2006-2011 by Dresden University of Technology
+
+   This software is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This software is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this software; if not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+'''
+
+
+import os
+from mercurial import Mercurial
+from cmake import CMake
+
+
+class Builder(object):
+  def __init__(self, package, srcdir, repodir, version):
+    object.__init__(self)
+
+    self.package    = package
+    self.srcdir     = os.path.join(srcdir, 'src', package)
+    self.builddir   = os.path.join(srcdir, 'build', package)
+    self.installdir = os.path.join(srcdir, 'install')
+    self.repodir    = os.path.join(repodir, package)
+    self.version    = version
+
+  def build(self, first = True):
+    print self.package
+    print '---------------------'
+    print ''
+
+    if first:
+      self.make_dirs()
+      self.clone()
+    else:
+      self.update()
+
+    self.install()
+
+
+  def make_dirs(self):
+    try:
+      os.makedirs(os.path.split(self.srcdir)[0])
+    except OSError:
+      pass
+
+    try:
+      os.makedirs(self.builddir)
+    except OSError:
+      pass
+ 
+
+  def clone(self):
+      _hg = Mercurial(self.srcdir)
+      print _hg.clone(self.repodir)
+
+
+  def update(self):
+      _hg = Mercurial(self.srcdir)
+      print _hg.update()
+
+
+  def install(self):
+      _cm = CMake(self.srcdir, self.installdir, self.builddir)
+      _cm.options.path('SARTURIS_PREFIX', self.installdir)
+      _cm.options.string('SARTURIS_MODULE_VERSION_MAJOR', self.version[0])
+      _cm.options.string('SARTURIS_MODULE_VERSION_MINOR', self.version[1])
+      _cm.options.string('SARTURIS_MODULE_VERSION_PATCH', self.version[2])      
+      print _cm.configure()
+      print _cm.make()
+      print _cm.install()
---
+++ trunk/build/executor.py
@@ -0,0 +1,44 @@
+'''
+
+   Sarturis is (C) Copyright 2006-2011 by Dresden University of Technology
+
+   This software is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This software is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this software; if not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+'''
+
+
+import os
+import shlex
+from subprocess import Popen, PIPE
+
+
+class Executor(object):
+  def __init__(self):
+    object.__init__(self)
+
+
+  def execute(self, cmd, arg, dir):
+    print '%s%s' % (cmd, arg)
+    print ''
+
+    _exe = shlex.split('%s%s' % (cmd, arg))
+    _cwd = os.getcwd()
+    os.chdir(dir)
+    _prc = Popen(_exe, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    _out, _err = _prc.communicate()
+    os.chdir(_cwd)
+
+    return (_out.rstrip().strip(), _err.rstrip().strip())
